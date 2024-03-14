35fe41eb0e142b94813f3b9cba5285cbcad16d6f
WanghaMods <132218157+Arwangha@users.noreply.github.com>
2024-03-14 14:30:26 +0100

    Fixing compilation errors

--- PiiXeLGame/Assets/Script/Entities/Entity.cs
+++ PiiXeLGame/Assets/Script/Entities/Entity.cs
@@ -1,48 +1,52 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Entity : Script.GridSystem.GridObject
+namespace Script.Entities
 {
-    public static int TOTAL_ACTION_BAR { get; private set; } = 50;    //
-    [SerializeField] private float actionFrequency;
-    [SerializeField] private float movementSpeed;
-    private float theta;
-
-    private Vector2 prevPos;
-    private bool isMoving = false;
-    public float actionBar { get; private set; } = 0;
-    private float currRotation;
-
-    private List<IAction> moveset;
-    abstract public void doActionInMoveset();
-    private void Start()
+    public abstract class Entity : Script.GridSystem.GridObject
     {
-        if (grid.gridType == Script.GridSystem.GridType.Square) theta = 2 * Mathf.PI / 4;
-        if (grid.gridType == Script.GridSystem.GridType.Triangle) theta = 2 * Mathf.PI / 3;
-        if (grid.gridType == Script.GridSystem.GridType.Hexagon) theta = 2 * Mathf.PI / 6;
-    }
+        public static int TOTAL_ACTION_BAR { get; private set; } = 50; //
+        [SerializeField] private float actionFrequency;
+        [SerializeField] private float movementSpeed;
+        private float theta;
 
-    public void Rotate(int nbFace) 
-    {
-        float ;
-        currRotation += theta * nbFace;
-    }
-    public void MoveForward()
-    {
-        isMoving = true;
-    }
+        private Vector2 prevPos;
+        private bool isMoving = false;
+        public float actionBar { get; private set; } = 0;
+        private float currRotation;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (isMoving && Vector2.Distance(prevPos, transform.position) < grid.cellSize)
+        private List<IAction> moveset;
+        abstract public void doActionInMoveset();
+
+        private void Start()
         {
-            transform.position += new Vector3() * movementSpeed;
+            if (grid.gridType == Script.GridSystem.GridType.Square) theta = 2 * Mathf.PI / 4;
+            if (grid.gridType == Script.GridSystem.GridType.Triangle) theta = 2 * Mathf.PI / 3;
+            if (grid.gridType == Script.GridSystem.GridType.Hexagon) theta = 2 * Mathf.PI / 6;
         }
-        else
+
+        public void Rotate(int nbFace)
         {
-            isMoving = false;
-            ForcePositionOntoGrid();
+            currRotation += theta * nbFace;
+        }
+
+        public void MoveForward()
+        {
+            isMoving = true;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isMoving && Vector2.Distance(prevPos, transform.position) < grid.cellSize)
+            {
+                transform.position += new Vector3() * movementSpeed;
+            }
+            else
+            {
+                isMoving = false;
+                ForcePositionOntoGrid();
+            }
         }
     }
 }
