57528a67a4700969a43ed09e9c62e3b43fdcd153
Simon LE FLOCH <132218157+Arwangha@users.noreply.github.com>
2024-04-25 17:19:51 +0200

    Allow Equipment to have fixed slots

--- PiiXeLGame/Assets/Script/Inventory/Inventory.cs
+++ PiiXeLGame/Assets/Script/Inventory/Inventory.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -10,17 +12,20 @@ namespace Script.Inventory
         [SerializeField] private float originY;
         [SerializeField] private float originEquipmentX;
         [SerializeField] private float originEquipmentY;
-        [SerializeField] private float infoDisplayX;
-        [SerializeField] private float infoDisplayY;
+        [SerializeField] private EquipmentItem emptySlot;
+        /*[SerializeField] private float infoDisplayX;
+        [SerializeField] private float infoDisplayY;*/
         [SerializeField] private GameObject selector;
         [SerializeField] private TMP_Text itemDescriptionText;
-        public int bagSize;
+        [SerializeField] private TMP_Text itemNameText;
+        //public int bagSize;
         public int colCount;
         public Vector2 itemSize;
         private Vector2 _itemPos;
         private Item _selectedItem;
         private List<Item> _bag;
-        private List<EquipmentItem> _equipment;
+        private Dictionary<EquipmentItem.Body, EquipmentItem> _equipmentDic;
+        private Dictionary<EquipmentItem.Body, EquipmentItem> _emptyEquipmentDic;
         private SpriteRenderer _selectorRenderer;
         private SpriteRenderer _spriteRenderer;
         private Transform _selectTransform;
@@ -33,12 +38,23 @@ namespace Script.Inventory
         private void Start()
         {
             _bag = new List<Item>();
-            _equipment = new List<EquipmentItem>();
+            _equipmentDic = new Dictionary<EquipmentItem.Body, EquipmentItem>();
+            _emptyEquipmentDic = new Dictionary<EquipmentItem.Body, EquipmentItem>();
             _itemPos = new Vector2();
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _spriteRenderer.enabled = false;
             _selectTransform = selector.GetComponent<Transform>();
             _selectorRenderer = selector.GetComponent<SpriteRenderer>();
+            foreach (EquipmentItem.Body equipmentType in Enum.GetValues(typeof(EquipmentItem.Body)))
+            {
+                EquipmentItem emptyEquipment = Instantiate(emptySlot);
+                emptyEquipment.itemName = "";
+                emptyEquipment.description = $"Slot for {equipmentType} equipment";
+                emptyEquipment.emptySlot = true;
+                Debug.Log(_equipmentDic);
+                _equipmentDic.Add(equipmentType, emptyEquipment);
+                _emptyEquipmentDic.Add(equipmentType, emptyEquipment);
+            }
         }
 
         public void AddToInventory(Item newItem, int amount = 1)
@@ -73,14 +89,13 @@ namespace Script.Inventory
                     {
                         if (_bag.Count != 0)
                         {
-                            if(_selectedPosInBag == _bag.Count) _selectedPosInBag -= 1;
+                            if (_selectedPosInBag == _bag.Count) _selectedPosInBag -= 1;
                             _selectedItem = _bag[_selectedPosInBag];
                         }
+
                         ChangeSelection(0, 0);
                     }
                 }
-
-                if (_bag.Count == 0 && _equipment.Count == 0) _selectorRenderer.enabled = false;
             }
 
             if (_isDisplayed) DrawInventory(_firstDrawnLine);
@@ -88,35 +103,35 @@ namespace Script.Inventory
 
         public void RemoveEquipment(EquipmentItem equippedItem)
         {
-            if (_equipment.Contains(equippedItem)) _equipment.Remove(equippedItem);
-            AddToInventory(equippedItem);
-            if (_isDisplayed)
+            if (equippedItem.emptySlot) return;
+            EquipmentItem.Body equipmentType = equippedItem.bodypart;
+            if (_equipmentDic[equipmentType] == equippedItem)
             {
-                DrawInventory(_firstDrawnLine);
-                if (_selectedPosInEquipment == _equipment.Count && _equipment.Count != 0)
-                {
-                    _selectedPosInEquipment -= 1;
-                }
-                
-                ChangeSelection(0, 0);
+                _equipmentDic[equipmentType] = _emptyEquipmentDic[equipmentType];
+                AddToInventory(equippedItem);
             }
+
+            if (!_isDisplayed) return;
+            DrawInventory(_firstDrawnLine);
+            ChangeSelection(0, 0);
         }
 
         public void AddOrReplaceEquipment(EquipmentItem equippedItem)
         {
+            EquipmentItem.Body equipmentType = equippedItem.bodypart;
             if (_bag.Contains(equippedItem))
             {
                 RemoveFromInventory(equippedItem);
-                foreach (EquipmentItem equipment in _equipment)
+                foreach (var equipment in _equipmentDic)
                 {
-                    if (equipment.bodypart == equippedItem.bodypart)
+                    if (equipment.Value.bodypart == equippedItem.bodypart)
                     {
-                        RemoveEquipment(equipment);
+                        RemoveEquipment(equipment.Value);
                         break;
                     }
                 }
 
-                _equipment.Add(equippedItem);
+                _equipmentDic[equipmentType] = equippedItem;
             }
 
             if (_isDisplayed) DrawInventory(_firstDrawnLine);
@@ -130,9 +145,9 @@ namespace Script.Inventory
             int iter = 0;
             _itemPos.x = originEquipmentX;
             _itemPos.y = originEquipmentY;
-            foreach (EquipmentItem possessedEquipment in _equipment)
+            foreach (var possessedEquipment in _equipmentDic)
             {
-                possessedEquipment.DrawItemInIventory(_itemPos);
+                possessedEquipment.Value.DrawItemInIventory(_itemPos);
                 _itemPos.y += itemSize.y;
             }
 
@@ -161,14 +176,13 @@ namespace Script.Inventory
             }
 
             itemDescriptionText.enabled = true;
+            itemNameText.enabled = true;
             if (_selectedItem)
             {
                 _selectTransform.position = _selectedItem.transform.position;
-                if (!(_bag.Count == 0 && _equipment.Count == 0))
-                {
-                    _selectorRenderer.enabled = true;
-                    itemDescriptionText.text = _selectedItem.description;
-                }
+                _selectorRenderer.enabled = true;
+                itemDescriptionText.text = _selectedItem.description;
+                itemNameText.text = _selectedItem.itemName;
             }
         }
 
@@ -182,22 +196,23 @@ namespace Script.Inventory
                 possessedItem.HideItem();
             }
 
-            foreach (EquipmentItem possessedEquipment in _equipment)
+            foreach (var possessedEquipment in _equipmentDic)
             {
-                possessedEquipment.HideItem();
+                possessedEquipment.Value.HideItem();
             }
 
             itemDescriptionText.enabled = false;
+            itemNameText.enabled = false;
         }
 
         public void UseSelectedItem()
         {
             if (_selectedItem is EquipmentItem)
             {
-                if(_selectedIsEquipment) RemoveEquipment((EquipmentItem)_selectedItem);
+                if (_selectedIsEquipment) RemoveEquipment((EquipmentItem)_selectedItem);
                 else AddOrReplaceEquipment((EquipmentItem)_selectedItem);
             }
-            
+
             if (_selectedItem)
             {
                 _selectedItem.UseItem();
@@ -206,7 +221,7 @@ namespace Script.Inventory
 
         public void ChangeSelection(int verticalAmount, int horizontalAmount)
         {
-            int equipmentAmount = _equipment.Count;
+            int equipmentAmount = _equipmentDic.Count;
             int itemAmount = _bag.Count;
             int lastRawAmount = itemAmount % colCount;
             int nonModifiedVerticalPos = verticalAmount;
@@ -233,17 +248,17 @@ namespace Script.Inventory
 
             int itemsInLine = (verticalAmount == itemAmount / colCount ? lastRawAmount : colCount) + 1;
             if (equipmentAmount == 0) itemsInLine -= 1;
-            
+
             int curHorPos = _selectedIsEquipment ? itemsInLine - 1 : _selectedPosInBag % colCount;
             horizontalAmount = (horizontalAmount % itemsInLine + curHorPos) % itemsInLine;
             if (horizontalAmount < 0) horizontalAmount += itemsInLine;
             _selectedIsEquipment = (horizontalAmount == itemsInLine - 1 && equipmentAmount != 0) || itemAmount == 0;
-            
+
             if (_selectedIsEquipment)
             {
                 _selectedPosInEquipment = (nonModifiedVerticalPos + _selectedPosInEquipment) % equipmentAmount;
                 if (_selectedPosInEquipment < 0) _selectedPosInEquipment += equipmentAmount;
-                _selectedItem = _equipment[_selectedPosInEquipment];
+                _selectedItem = _equipmentDic.ElementAt(_selectedPosInEquipment).Value;
                 _selectedPosInBag = _selectedPosInEquipment * colCount;
                 if (_selectedPosInBag >= itemAmount) _selectedPosInBag = itemAmount - 1;
             }
@@ -255,8 +270,9 @@ namespace Script.Inventory
                 _selectedItem = _bag[_selectedPosInBag];
                 _selectedPosInEquipment = verticalAmount >= equipmentAmount ? equipmentAmount - 1 : verticalAmount;
             }
-            
+
             itemDescriptionText.text = _selectedItem.description;
+            itemNameText.text = _selectedItem.itemName;
             if (_isDisplayed) DrawInventory(_firstDrawnLine);
         }
     }
